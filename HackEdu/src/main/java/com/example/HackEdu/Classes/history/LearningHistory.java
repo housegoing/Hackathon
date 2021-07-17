144358787f633a51ac0dc270c61659629cca48ce
Christina <xxychristina@gmail.com>
2021-07-17 17:34:39 +0800

    user and history

--- HackEdu/src/main/java/com/example/HackEdu/Classes/history/LearningHistory.java
+++ HackEdu/src/main/java/com/example/HackEdu/Classes/history/LearningHistory.java
@@ -1,9 +1,35 @@
 package com.example.HackEdu.Classes.history;
 
+import com.example.HackEdu.Classes.user.User;
+
+import javax.persistence.*;
 import java.time.LocalDate;
 
-class LearningHistory {
-    private String id;
+@Entity
+@Table(name = "learning_history")
+public class LearningHistory {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
     private LocalDate dateAccess;
     private String contentId;
+
+    @OneToOne(mappedBy = "learningHistory")
+    private User user;
+
+    public LearningHistory(LocalDate dateAccess, String contentId, User user) {
+        this.dateAccess = dateAccess;
+        this.contentId = contentId;
+        this.user = user;
+    }
+
+    public LearningHistory(Long id, LocalDate dateAccess, String contentId, User user) {
+        this.id = id;
+        this.dateAccess = dateAccess;
+        this.contentId = contentId;
+        this.user = user;
+    }
+
+    public LearningHistory() {
+    }
 }
