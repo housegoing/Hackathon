5de77a9008650d3eb4a7074169335a3c2b6693b5
table1065 <41575967+table1065@users.noreply.github.com>
2021-07-17 22:31:52 +1000

    Update HackEduApplication.java

--- HackEdu/src/main/java/com/example/HackEdu/HackEduApplication.java
+++ HackEdu/src/main/java/com/example/HackEdu/HackEduApplication.java
@@ -33,8 +33,8 @@ public class HackEduApplication implements CommandLineRunner {
 		learningHistory.setDateAccess(LocalDate.of(2021, 7, 17));
 		learningHistory.setContentId("12586");
 
-		learningHistory.setUser(user);
 		user.setLearningHistory(learningHistory);
+		learningHistory.setUser(user);
 
 		userRepository.save(user);
 	}
