2fa66af58cc3c329c4f25783c5577a4834d8daaf
table1065 <41575967+table1065@users.noreply.github.com>
2021-07-17 22:56:38 +1000

    Tested and fixed

--- HackEdu/src/main/java/com/example/HackEdu/HackEduApplication.java
+++ HackEdu/src/main/java/com/example/HackEdu/HackEduApplication.java
@@ -1,5 +1,7 @@
 package com.example.HackEdu;
 
+import com.example.HackEdu.Classes.article.Article;
+import com.example.HackEdu.Classes.article.ArticleRepository;
 import com.example.HackEdu.Classes.history.LearningHistory;
 import com.example.HackEdu.Classes.history.LearningHistoryRepository;
 import com.example.HackEdu.Classes.user.User;
@@ -23,19 +25,24 @@ public class HackEduApplication implements CommandLineRunner {
 	@Autowired
 	private LearningHistoryRepository learningHistoryRepository;
 
+	@Autowired
+	private ArticleRepository articleRepository;
+
 	@Override
 	public void run(String... args) throws Exception {
-		User user = new User("6112121212");
+//		User user = new User("6112121212");
 //		user.setPhoneNumber("6112121212");
+//
+//		Article article = new Article();
+//		articleRepository.save(article);
 //
 //		LearningHistory learningHistory = new LearningHistory();
 //		learningHistory.setDateAccess(LocalDate.of(2021, 7, 17));
-//		learningHistory.setContentId("12586");
+//		learningHistory.setArticle(article);
 //
 //		learningHistory.setUser(user);
-//		user.setLearningHistory(learningHistory);
-//
-		userRepository.save(user);
+//		userRepository.save(user);
+//		learningHistoryRepository.save(learningHistory);
 	}
 
 }
