e811260092e343e2a663d9dc9f649faae40c8056
Christina <xxychristina@gmail.com>
2021-07-17 20:54:49 +0800

    getArticlebyTopic

--- HackEdu/src/main/java/com/example/HackEdu/Classes/article/ArticleRepository.java
+++ HackEdu/src/main/java/com/example/HackEdu/Classes/article/ArticleRepository.java
@@ -5,8 +5,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface ArticleRepository extends JpaRepository<Article, Long> {
-    List<Article> findArticleByCourse(Course course);
+    List<Article> findArticleByCourse(Optional<Course> course);
 }
--- HackEdu/src/main/java/com/example/HackEdu/Classes/course/CourseRepository.java
+++ HackEdu/src/main/java/com/example/HackEdu/Classes/course/CourseRepository.java
@@ -3,7 +3,9 @@ package com.example.HackEdu.Classes.course;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface CourseRepository extends JpaRepository<Course, Long> {
-
+    Optional<Course> findCourseByTopic(String course);
 }
