c7af17b7ae1edc68cd11899003f002a26edb52e8
Christina <xxychristina@gmail.com>
2021-07-17 22:19:05 +0800

    updateLearning

--- HackEdu/src/main/java/com/example/HackEdu/Classes/history/LearningHistoryRepository.java
+++ HackEdu/src/main/java/com/example/HackEdu/Classes/history/LearningHistoryRepository.java
@@ -1,9 +1,17 @@
 package com.example.HackEdu.Classes.history;
 
+import com.example.HackEdu.Classes.article.Article;
+import com.example.HackEdu.Classes.user.User;
+import com.example.HackEdu.Classes.video.Video;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface LearningHistoryRepository extends JpaRepository<LearningHistory, Long> {
-
+    Optional<LearningHistory>
+        findLearningHistoryByArticleAndUser(Article article, User user);
+    Optional<LearningHistory>
+        findLearningHistoryByVideoAndUser(Video video, User user);
 }
