89c78e9e7a26f6d0505a76d13ea1743fcf0c748e
Jacob <41574808+cheng0210@users.noreply.github.com>
2021-07-18 02:23:29 +1000

    update list command

--- HackEdu/src/main/java/com/example/HackEdu/HackEduController.java
+++ HackEdu/src/main/java/com/example/HackEdu/HackEduController.java
@@ -1,4 +1,8 @@
 package com.example.HackEdu;
+import com.example.HackEdu.Classes.article.Article;
+import com.example.HackEdu.Classes.article.ArticleService;
+import com.example.HackEdu.Classes.course.Course;
+import com.example.HackEdu.Classes.course.CourseService;
 import com.example.HackEdu.Classes.user.User;
 import com.example.HackEdu.Classes.user.UserService;
 import com.example.HackEdu.Twilio.SmsRequest;
@@ -8,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.Locale;
 
 @RestController
 public class HackEduController {
@@ -16,10 +21,16 @@ public class HackEduController {
 
     private UserService userService;
 
+    private CourseService courseService;
+
+    private ArticleService articleService;
+
     @Autowired
-    public HackEduController(Service service, UserService userService) {
+    public HackEduController(Service service, UserService userService, CourseService courseService, ArticleService articleService) {
         this.service = service;
         this.userService = userService;
+        this.courseService = courseService;
+        this.articleService = articleService;
     }
 
     @RequestMapping("/smsSender")
@@ -33,49 +44,59 @@ public class HackEduController {
         String phoneNumber = from.substring(1);
         userService.addNewUser(new User(phoneNumber));
 
-        String[] inputMsg = body.split(" ");
+        String[] inputMsg = body.toLowerCase().split(" ");
 
         System.out.println(inputMsg);
 
-        String response = new String();
+        String responseMsg;
 
-        String help = "Welcome to HackEdu!" + '\n' +
+        String help = '\n' + "Welcome to HackEdu!" + '\n' +
                 "Use 'list topic' to explore your interested topics." + '\n' +
-                "Use 'list topicIndex course' to explore courses in your interested topic." + '\n' +
-                "Use 'list topicIndex courseIndex content' to explore contents in that specified course." + '\n';
+                "Use 'list topicIndex course' to explore courses in your interested topic." + '\n';
 
-        String error = "Invalid input! Type 'help' to get instructions.";
+        String error = '\n' + "Invalid input! Type 'e-help' to get instructions.";
 
         switch(inputMsg[0]){
-            case "help":
-                response = help;
+            case "e-help":
+                responseMsg = help;
                 break;
             case "list":
-                if(inputMsg[1] == "topic"){
-                    // todo list all topics
-                    response = "Index-----Topic" + '\n';
+                System.out.println(inputMsg[1]);
+                if(inputMsg[1].equals("topic")){
+                    responseMsg = "Topics:" + '\n' + "Index---Topic" + '\n';
+                    int count = 1;
+                    for(Course c: courseService.getCourse()){
+                        responseMsg = responseMsg + "  #" + String.valueOf(count) + "---" + c.getTopic() + '\n';
+                        count = count + 1;
+                    }
                 }else{
                     try{
-                        int topicIndex = Integer.parseInt(inputMsg[1]);
+                        int topicIndex = Integer.parseInt(inputMsg[1].substring(1));
 
-                        if(inputMsg[2] == "course"){
+                        if(inputMsg[2].equals("course") && topicIndex > 0){
                             //todo list all courses in that topic
-                            response = "You are viewing Topic {NAME}:" + '\n' +
-                                    "Index-----Course" + '\n';
+                            responseMsg = "You are viewing Topic " + courseService.getCourse().get(topicIndex-1).getTopic() +":" + '\n' +
+                                    "Index --- Type --- Course" + '\n';
+                            int count = 1;
+                            for(Article a: articleService.getArticalByTopic(courseService.getCourse().get(topicIndex-1).getTopic())){
+                                responseMsg = responseMsg + "  #" + String.valueOf(count) + "  ---  "+ "Article  ---  " + a.getName() + '\n';
+                                count = count + 1;
+                            }
+
                         }else{
-                            response = error;
+                            responseMsg = error;
                         }
                     }
                     catch (NumberFormatException ex){
-                        response = error;
+                        responseMsg = error;
                     }
                 }
                 break;
             default:
-                response = error;
+                responseMsg = error;
+                break;
         }
-
-        return response;
+        return responseMsg;
     }
 
 }
