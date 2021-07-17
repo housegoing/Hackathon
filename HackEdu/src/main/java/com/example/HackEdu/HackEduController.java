10d641d28742394bf35e95da71f701a31158d59d
Jacob <41574808+cheng0210@users.noreply.github.com>
2021-07-18 00:48:24 +1000

    update dispatch

--- HackEdu/src/main/java/com/example/HackEdu/HackEduController.java
+++ HackEdu/src/main/java/com/example/HackEdu/HackEduController.java
@@ -1,4 +1,6 @@
 package com.example.HackEdu;
+import com.example.HackEdu.Classes.user.User;
+import com.example.HackEdu.Classes.user.UserService;
 import com.example.HackEdu.Twilio.SmsRequest;
 import com.twilio.twiml.MessagingResponse;
 import com.twilio.twiml.messaging.Message;
@@ -11,6 +13,8 @@ import javax.validation.Valid;
 public class HackEduController {
 
     private final Service service;
+
+    private UserService userService;
 
     @Autowired
     public HackEduController(Service service) {
@@ -25,7 +29,52 @@ public class HackEduController {
     @RequestMapping("/sms")
     @ResponseBody
     public String smsDispatch(@RequestParam("From") String from, @RequestParam("Body") String body){
-        return body;
+        String phoneNumber = from.substring(1);
+        userService.addNewUser(new User(phoneNumber));
+
+        String[] inputMsg = body.split(" ");
+
+        System.out.println(inputMsg);
+
+        String response = new String();
+
+        String help = "Welcome to HackEdu!" + '\n' +
+                "Use 'list topic' to explore your interested topics." + '\n' +
+                "Use 'list topicIndex course' to explore courses in your interested topic." + '\n' +
+                "Use 'list topicIndex courseIndex content' to explore contents in that specified course." + '\n';
+
+        String error = "Invalid input! Type 'help' to get instructions.";
+
+        switch(inputMsg[0]){
+            case "help":
+                response = help;
+                break;
+            case "list":
+                if(inputMsg[1] == "topic"){
+                    // todo list all topics
+                    response = "Index-----Topic" + '\n';
+                }else{
+                    try{
+                        int topicIndex = Integer.parseInt(inputMsg[1]);
+
+                        if(inputMsg[2] == "course"){
+                            //todo list all courses in that topic
+                            response = "You are viewing Topic {NAME}:" + '\n' +
+                                    "Index-----Course" + '\n';
+                        }else{
+                            response = error;
+                        }
+                    }
+                    catch (NumberFormatException ex){
+                        response = error;
+                    }
+                }
+                break;
+            default:
+                response = error;
+        }
+
+        return response;
     }
 
 }
