98d1d95dfe2835c07246f961e115aaa856dae542
table1065 <41575967+table1065@users.noreply.github.com>
2021-07-17 15:47:59 +1000

    Created user and publisher

---
+++ HackEdu/src/main/java/com/example/HackEdu/Classes/Publisher.java
@@ -0,0 +1,11 @@
+package com.example.HackEdu.Classes;
+
+public class Publisher {
+
+    private final String email;
+    private String firstName;
+    private String lastName;
+
+    public Publisher() {
+    }
+}
---
+++ HackEdu/src/main/java/com/example/HackEdu/Classes/User.java
@@ -0,0 +1,20 @@
+package com.example.HackEdu.Classes;
+
+public class User   {
+
+    private String phoneNumber;
+    private String firstName;
+    private String lastName;
+
+    public User() {
+    }
+
+    public User(String phoneNumber, String firstName, String lastName) {
+        this.phoneNumber = phoneNumber;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+
+
+}
